ec4de64dede216e15d78f33c72b57ed388f7311d
TimTree <TimTree@users.noreply.github.com>
2021-03-08 16:32:36 -0800

    Add multithreading

--- critterNames.py
+++ critterNames.py
@@ -1,11 +1,12 @@
 import time
+import concurrent.futures
 import argparse
 import requests
 import json
 import re
 
 print('--------------------------------------------')
-print('BOX CRITTERS NAME CHECKER v1.0')
+print('BOX CRITTERS NAME CHECKER v2.0')
 print('github.com/TimTree/box-critters-name-checker')
 print('--------------------------------------------')
 
@@ -16,13 +17,18 @@ parser.add_argument(
     '-i',
     '--input',
     default='test.txt',
-    help='Input file of nicknames to test. If not specified, \
-        the file used is "test.txt".')
+    help='Input file of nicknames to test. Defaults to "test.txt".')
+parser.add_argument(
+    '-th',
+    '--thread',
+    default='multi',
+    help='Choose whether to use single threading "single" or multithreading \
+        "multi" to check nicknames. Defaults to "multi".')
 args = parser.parse_args()
 
 apiURL = 'https://titleId.playfabapi.com/Client/RegisterPlayFabUser'
 headers = {'Content-Type': 'application/json'}
-count = 1
+count = 0
 untakenNames = []
 try:
     with open(args.input) as f:
@@ -32,15 +38,17 @@ except Exception as e:
     print(f'ERROR: {e}')
     print('Is your terminal in the same directory as critterNames.py?')
     quit()
-for name in allNames:
+
+
+def checkNames(name):
     nickname = name.strip()
-    print(f'Checking nickname {count}/{totalNames}: {nickname} ... ', end='')
-    # Check if nickname is valid (3-5 characters, letters/numbers/spaces only)
+    returnPrint = f'{nickname} ... '
+    # Check if nickname is valid (3-5 characters, letters/numbers/spaces
+    # only)
     if (len(nickname) < 3 or len(nickname) >
             25 or not re.match('^[a-zA-Z0-9 ]*$', nickname)):
-        print('[X] (Invalid)')
-        count += 1
-        continue
+        returnPrint += '[X] (Invalid)'
+        return returnPrint
     # If nickname is vaild, send to API to see if it's taken
     dictToSend = {
         'TitleId': '5417',
@@ -48,19 +56,37 @@ for name in allNames:
         'Password': 'doesntmatter',
         'RequireBothUsernameAndEmail': False,
     }
-    res = requests.post(apiURL, data=json.dumps(dictToSend), headers=headers)
+    res = requests.post(apiURL, data=json.dumps(dictToSend),
+                        headers=headers)
+    while json.loads(res.text)['error'] == 'APIClientRequestRateLimitExceeded':
+        res = requests.post(apiURL, data=json.dumps(dictToSend),
+                            headers=headers)
     if json.loads(res.text)['error'] == 'InvalidParams':
         untakenNames.append(nickname)
-        print('[✓]')
+        returnPrint += '[✓]'
     else:
-        print('[X] (Taken)')
-    count += 1
+        returnPrint += '[X] (Taken)'
+    return returnPrint
+
+
+if args.thread.lower() == 'single':  # single threading
+    for name in allNames:
+        count += 1
+        print(f'Checked nickname {count}/{totalNames}: {checkNames(name)}')
+else:  # multithreading
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = [executor.submit(checkNames, name) for name in allNames]
+        for c in concurrent.futures.as_completed(results):
+            count += 1
+            print(f'Checked nickname {count}/{totalNames}: {c.result()}')
+
 print('')
 print('---')
 print(f'Found {len(untakenNames)} untaken nickname(s)')
 print('---')
-for untakenName in untakenNames:
+for untakenName in sorted(untakenNames):
     print(untakenName)
 
 finish = time.perf_counter()
+print('')
 print(f'Finished in {round(finish-start, 2)} second(s)')
